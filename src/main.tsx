2fd14e4aee3fef5e3b35d997b0c433a6f7ade329
wendelsantosd <wendelwcsantos@gmail.com>
2023-09-03 17:53:39 -0300

    chore: remove unused

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Index } from ".";
-import { DataContext } from "./contexts/data";
 import { LanguageContext } from "./contexts/language";
 import { ThemeContext } from "./contexts/theme";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  // <DataContext>
-    <LanguageContext>
-      <ThemeContext>
-        <Index />
-      </ThemeContext>
-    </LanguageContext>
-  // </DataContext>
+  <LanguageContext>
+    <ThemeContext>
+      <Index />
+    </ThemeContext>
+  </LanguageContext>
 );
